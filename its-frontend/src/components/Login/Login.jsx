c1d22700a48fa2456c59c8b43979ca26c9561c1e
aquib2502 <aquibhingwala@gmail.com>
2025-06-19 15:33:46 +0530

    kuch bhi

--- its-frontend/src/components/Login/Login.jsx
+++ its-frontend/src/components/Login/Login.jsx
@@ -14,12 +14,7 @@ function Login() {
 
     const token = localStorage.getItem("userToken");
 
-
-   
-   
-   
-   
-    try {
+  try {
       const response = await axios.post(
         "http://localhost:5000/api/user/login",
         { email, password },
